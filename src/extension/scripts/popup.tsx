a54d2cecb970e3853c9659741bc55ec54e5200be
Warren Whipple <w@warrenwhipple.com>
2025-07-27 14:13:52 +0200

    remove stray console log

--- src/extension/scripts/popup.tsx
+++ src/extension/scripts/popup.tsx
@@ -6,8 +6,6 @@ import { PageLink } from "../components/page-link";
 
 console.log("[TodoSync] extension popup loading on", location?.href);
 
-console.log(chrome?.runtime.getURL("page.html"));
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <h1>Todo Sync - Popup</h1>
