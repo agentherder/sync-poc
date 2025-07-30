922937979588d96ed0532d4d6b8e9cd351eea086
Warren Whipple <w@warrenwhipple.com>
2025-07-30 06:38:43 +0200

    triplit persistence and shared worker

--- triplit/client.ts
+++ triplit/client.ts
@@ -1,7 +1,10 @@
-import { TriplitClient } from "@triplit/client";
+import { WorkerClient } from "@triplit/client/worker-client";
 import { schema } from "./schema";
+import workerUrl from "@triplit/client/worker-client-operator?url";
 
-export const triplit = new TriplitClient({
+export const triplit = new WorkerClient({
+  workerUrl,
+  storage: "indexeddb",
   schema,
   serverUrl: import.meta.env.VITE_TRIPLIT_SERVER_URL,
   token: import.meta.env.VITE_TRIPLIT_TOKEN,
