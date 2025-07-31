d1604fd1fdefac52fc0cbb7bc916d76357383fa8
Warren Whipple <w@warrenwhipple.com>
2025-07-31 22:54:15 +0200

    persist to local storage

--- src/shared/todo-store.ts
+++ src/shared/todo-store.ts
@@ -1,5 +1,11 @@
 import { createStore } from "tinybase";
+import { createLocalPersister } from "tinybase/persisters/persister-browser";
 
-export const todoStore = createStore().setTables({
-  todos: {},
+export const todoStore = createStore().setTablesSchema({
+  todos: {
+    title: { type: "string" },
+    completed: { type: "boolean", default: false },
+  },
 });
+
+createLocalPersister(todoStore, "todo-poc-store").startAutoPersisting();
